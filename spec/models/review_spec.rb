0a9a0107bad94acf3a56372baa66321db56d9c43
bhopek <bhopek@atsisa.com>
2015-06-12 18:24:09 +0200

    +review validations

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,8 +4,8 @@ describe Review do
   describe 'validations' do
     it { should validate_presence_of :content }
     it { should validate_presence_of :rating }
+    # @q: why not: validate_presence_of :user
     it { should validate_presence_of :user_id }
     it { should belong_to :user }
-
   end
 end
