18dcd53f0bf4d81138750c3cf01a0940aa611dbf
bhopek <bhopek@gmail.com>
2015-06-14 02:41:39 +0200

    + review created by not signed in user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-
+  before_action :authenticate_user!
   expose(:review)
   expose(:product)
   expose(:category)
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -7,11 +7,21 @@ describe ReviewsController do
     let(:product) { create(:product) }
     let(:valid_params) { { content: 'content', rating: '1' } }
 
-    before { controller.stub(:current_user).and_return(user) }
+    context 'when user is signed in' do
+      before { controller.stub(:current_user).and_return(user) }
 
-    context 'with valid params' do
       subject { -> { post :create, review: valid_params, category_id: category.to_param, product_id: product.to_param } }
       it { is_expected.to change(Review, :count).by(1) }
+    end
+
+    context 'when user is not signed in' do
+      before { controller.stub(:current_user).and_return(nil) }
+
+      subject do
+        post :create, review: valid_params, category_id: category.to_param, product_id: product.to_param
+        response
+      end
+      it { is_expected.to redirect_to(new_user_session_path) }
     end
   end
 end
