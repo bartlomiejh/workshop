7464567574af0a9db4905a7ec3cfc45a8eb4a012
bhopek <bhopek@gmail.com>
2015-06-14 02:48:20 +0200

    - rubocop

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -5,12 +5,16 @@ describe ReviewsController do
     let(:user) { create(:user) }
     let(:category) { create(:category) }
     let(:product) { create(:product) }
-    let(:valid_params) { { content: 'content', rating: '1' } }
+    let(:valid_params) do
+      { review: { content: 'content', rating: '1' },
+        category_id: category.to_param,
+        product_id: product.to_param }
+    end
 
     context 'when user is signed in' do
       before { controller.stub(:current_user).and_return(user) }
 
-      subject { -> { post :create, review: valid_params, category_id: category.to_param, product_id: product.to_param } }
+      subject { -> { post :create, valid_params } }
       it { is_expected.to change(Review, :count).by(1) }
     end
 
@@ -18,7 +22,7 @@ describe ReviewsController do
       before { controller.stub(:current_user).and_return(nil) }
 
       subject do
-        post :create, review: valid_params, category_id: category.to_param, product_id: product.to_param
+        post :create, valid_params
         response
       end
       it { is_expected.to redirect_to(new_user_session_path) }
