554eac340cdee29e4779a9f2b59f7395756c6be9
bhopek <bhopek@gmail.com>
2015-06-17 22:50:15 +0200

    +can_edit_any_product?

--- spec/decorators/category_decorator_spec.rb
+++ spec/decorators/category_decorator_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe CategoryDecorator do
+  describe '#can_edit_any_product?' do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    let(:category) { described_class.new(create(:category)) }
+
+    subject { category.can_edit_any_product? user1 }
+    context 'when products empty' do
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when there is not owned product' do
+      let!(:product1) { create(:product, category: category, user: user2) }
+      let!(:product2) { create(:product, category: category, user: user2) }
+      it { is_expected.to be_falsey }
+    end
+
+    context 'when there is owned product' do
+      let!(:product1) { create(:product, category: category, user: user1) }
+      let!(:product2) { create(:product, category: category, user: user2) }
+      it { is_expected.to be_truthy }
+    end
+  end
 end
