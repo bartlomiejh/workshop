0c84b26fa16e1fc130efdb1eb033aa11c6074526
bhopek <bhopek@atsisa.com>
2015-06-19 10:02:52 +0200

    -index product action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :categories do
-    resources :products do
+    resources :products, except: :index do
       resources :reviews, only: [:create, :new]
     end
   end
