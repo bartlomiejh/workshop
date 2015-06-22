ab7ca23ac80d46334d3540624e200c18b408260d
bhopek <bhopek@gmail.com>
2015-06-23 01:56:44 +0200

    exposed user for profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  expose_decorated(:user) { current_user }
   expose_decorated(:reviews) { current_user.reviews.order(created_at: :desc).limit(5) }
 
   def show
