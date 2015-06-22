2e2c6fda2229faa27df4a66dc4ce1e95e08d300c
bhopek <bhopek@gmail.com>
2015-06-23 01:21:01 +0200

    seeds refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-5.times do |i|
-  u = User.create!(email: "user#{i}@a.com", firstname: "Firstname#{i}", lastname: "Lastname#{i}", password: 'Qwerty123')
-
-  c = Category.create!(name: "Category##{u.email.split('@').first}")
+def create_entries u
+  u_prefix = u.email.split('@').first
+  c = Category.create!(name: "name##{u_prefix}")
 
   product_params = {
-    title: "Title##{c.name}",
-    description: 'description',
-    price: [*1..10].sample,
-    category: c,
-    user: u
+      description: 'description',
+      category: c,
+      user: u
   }
-  p = Product.create!(product_params)
-
   review_params = {
-    content: 'content',
-    rating: [*1..5].sample,
-    user: u,
-    product: p
+      content: 'content',
+      user: u
   }
-
-  Review.create!(review_params)
+  3.times do |i|
+    p = Product.create!({ title: "title##{i}##{u_prefix}", price: [*1..10].sample }.merge(product_params))
+    10.times do |j|
+      Review.create!({ product: p, rating: [*1..5].sample, }.merge(review_params))
+    end
+  end
 end
-User.create!(email: "admin@a.com", firstname: 'AdminFirst', lastname: 'AdminLast', password: 'Qwerty123', admin: true)
+
+5.times do |i|
+  create_entries User.create!(email: "user#{i}@a.com", firstname: "Firstname#{i}", lastname: "Lastname#{i}", password: 'Qwerty123')
+end
+create_entries User.create!(email: "admin@a.com", firstname: 'AdminFirst', lastname: 'AdminLast', password: 'Qwerty123', admin: true)
+
 
